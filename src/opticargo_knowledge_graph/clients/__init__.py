03187896087cf51436f1dc602a73cae98ccfb874
unknown <satriahadiwiyana7@gmail.com>
2026-08-10 15:14:32 +0700

    fix(clients): align package exports dependency client with runtime contract

--- src/opticargo_knowledge_graph/clients/__init__.py
+++ src/opticargo_knowledge_graph/clients/__init__.py
@@ -1,7 +1,8 @@
-"""Client factory exports."""
+from .neo4j import Neo4jClient, create_neo4j_driver
+from .postgres import PostgresClient, create_postgres_connection
+from .redis_stream import RedisStreamClient, create_redis_client
 
-from opticargo_knowledge_graph.clients.neo4j import create_neo4j_driver
-from opticargo_knowledge_graph.clients.postgres import create_postgres_connection
-from opticargo_knowledge_graph.clients.redis_stream import create_redis_client
-
-__all__ = ["create_neo4j_driver", "create_postgres_connection", "create_redis_client"]
+__all__ = [
+    "Neo4jClient", "PostgresClient", "RedisStreamClient",
+    "create_neo4j_driver", "create_postgres_connection", "create_redis_client",
+]
