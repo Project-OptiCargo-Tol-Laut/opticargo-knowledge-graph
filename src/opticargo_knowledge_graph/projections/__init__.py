f92648dd5d13944fae94575e0f6c0a04a41cea58
unknown <satriahadiwiyana7@gmail.com>
2026-08-10 15:15:24 +0700

    feat(projection): align package exports graph projection behavior

--- src/opticargo_knowledge_graph/projections/__init__.py
+++ src/opticargo_knowledge_graph/projections/__init__.py
@@ -1,9 +1,4 @@
-"""Projection exports."""
-
-from opticargo_knowledge_graph.projections.registry import (
-    ProjectionRegistry,
-    default_projection_registry,
-)
-from opticargo_knowledge_graph.projections.service import ProjectionService
-
-__all__ = ["ProjectionRegistry", "ProjectionService", "default_projection_registry"]
+from .models import NodeRef, ProjectionInput, ProjectionPlan, RelationshipPlan
+from .registry import ProjectionRegistry, ProjectionSpec, default_projection_registry, get_projection_spec, normalize_entity_type, supported_entity_types
+from .service import ProjectionOutcome, ProjectionService, resolve_projection_target
+__all__ = ["NodeRef", "ProjectionInput", "ProjectionPlan", "RelationshipPlan", "ProjectionRegistry", "ProjectionSpec", "default_projection_registry", "get_projection_spec", "normalize_entity_type", "supported_entity_types", "ProjectionOutcome", "ProjectionService", "resolve_projection_target"]
